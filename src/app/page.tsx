ce76f6f2ecd042e81977e5800647e0d8c9c9b5f6
jevontrei <160815182+jevontrei@users.noreply.github.com>
2026-02-01 18:32:11 +1000

    add ig icon; just get it to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Page() {
-  return <h2 className="text-6xl">BVNT</h2>;
+  return (
+    <>
+      <h2 className="text-6xl">BVNT</h2>
+      <Link
+        href="https://www.instagram.com/benvontrei/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:grayscale-50"
+      >
+        <Image
+          src="/instagram-logo-facebook-2-svgrepo-com.svg"
+          alt="Instagram logo"
+          width={24}
+          height={24}
+        />
+      </Link>
+    </>
+  );
 }
